352bd35ba186ce5cc1b4068132bac4bf71da1a5c
Yusuke Nishizawa <36687490+zwt1n@users.noreply.github.com>
2018-09-06 15:48:22 +0900

    Update candidate_action_log.sql

--- candidate_action_log.sql
+++ candidate_action_log.sql
@@ -1,7 +1,8 @@
-/* データの定義
+/* Data set No.001 求職者アクションログ
+データの定義
 ** ビズリーチへのアクションは除く
 ** ESS/CRS含む
-** 直近13ヶ月のアクションデータ
+** 直近6ヶ月のアクションデータ ※ESSのスカウトデータ量が多い為、半年に絞る
 ** candidate_id : 求職者ID
 ** act_date : YYYY-MM-DD
 ** act_type : 0_ログイン, 1_興味がある, 2_PS, 3_応募, 4_HS, 5_内定, 6_決定
@@ -49,7 +50,6 @@ with cp as (
   where
     -- ビズリーチ除外
     cp.recruiter_company_id <> 1  
-
 ),
 
 tran as (
@@ -147,11 +147,11 @@ UNION ALL
         -- ビズリーチ除外
         and rcd.recruiter_company_id <> 1
     )
-
+  
 UNION ALL
 
   ------------------------------
-  -- 3_PS
+  -- 3_PS送信
   ------------------------------
   select
     -- PS件数をカウントする際は、tableau等でmessage_thread_idでdistinctかけること！
@@ -180,7 +180,7 @@ UNION ALL
           on mt.message_thread_id = m.message_thread_id
           and mt.scout_sent_tm = m.sent_tm
           and m.from_recruiter = 'TRUE'
-          and m.sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-13))
+          and m.sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-6))
         left join (
           select distinct
             mt.message_thread_id
@@ -199,7 +199,7 @@ UNION ALL
                   message m
                 where
                   from_recruiter ='FALSE'
-                  and m.sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-13))
+                  and m.sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-6))
                 group by
                   message_thread_id
             ) rep
@@ -207,12 +207,14 @@ UNION ALL
           where
             --プラスカ送信スレッドに限定する
             mt.create_trigger_type_cd = 'PSC'
+            --and mt.company_type_cd = 'CRS'
             --求職者がスカウトメッセージを開封している
             and mt.cn_scout_first_view_tm is not null
         )psrep on mt.message_thread_id = psrep.message_thread_id
     where
         mt.create_trigger_type_cd = 'PSC' -- PSのみに絞る
-        and mt.scout_sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-13))
+        --and mt.company_type_cd = 'CRS'
+        and mt.scout_sent_tm >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-6))
         and mt.recruiter_company_id <> 1 
   )
     
@@ -329,9 +331,153 @@ UNION ALL
   WHERE
     c.candidate_status_cd = 'LFT'
     and c.upd_datetime >= date_trunc('month',add_months(trunc(convert_timezone('JST', getdate())),-13))
+),
+
+c2 as (
+  with c as (
+    select
+      c.candidate_id
+      ,c.ins_datetime
+      ,c.gender_cd
+      ,DATEDIFF(YEAR, c.birth_date, GETDATE()) + (CASE WHEN DATE_PART (doy, c.birth_date) > DATE_PART (doy, GETDATE()) THEN - 1 ELSE 0 END) as age
+      ,msi.school_rank_cd
+      ,cr.candidate_rank_cd
+      ,ma.area_name
+      ,case when (split_part(cpi.tracking_code,'\_',1) is null or split_part(cpi.tracking_code,'\_',1) in ('Organic','app')) 
+            then 'Organic' else split_part(cpi.tracking_code,'\_',1) end as trcd
+      ,case when mjt.JOB_GROUP_TYPE_CD = 'JA' then '営業'
+            when mjt.JOB_GROUP_TYPE_CD = 'JB' then 'IT・WEB・エンジニア'
+            when mjt.JOB_GROUP_TYPE_CD = 'JC' then 'マーケティング'
+            when mjt.JOB_GROUP_TYPE_CD = 'JD' then 'メーカー'
+            when mjt.JOB_GROUP_TYPE_CD = 'JE' then '経営・企画・管理'
+            when mjt.JOB_GROUP_TYPE_CD = 'JF' then '販売・サービス'
+            when mjt.JOB_GROUP_TYPE_CD = 'JG' then '資材・購買・物流'
+            when mjt.JOB_GROUP_TYPE_CD = 'JH' then '建設・土木'
+            when mjt.JOB_GROUP_TYPE_CD = 'JI' then 'コンサルタント'
+            when mjt.JOB_GROUP_TYPE_CD = 'JJ' then '金融・不動産'
+            when mjt.JOB_GROUP_TYPE_CD = 'JK' then 'メディカル'
+            when mjt.JOB_GROUP_TYPE_CD = 'JL' then '専門職'
+            when mjt.JOB_GROUP_TYPE_CD = 'JM' then 'クリエイティブ'
+            when mjt.JOB_GROUP_TYPE_CD = 'JN' then 'その他'
+            else null end as JOB_GROUP_TYPE_NAME  
+      ,mjt.job_type_name
+      ,case when mbt.business_type_group_cd = 'IA' then 'IT・インターネット・ゲーム・通信'
+            when mbt.business_type_group_cd = 'IB' then 'メーカー'
+            when mbt.business_type_group_cd = 'IC' then 'コンサルティング'
+            when mbt.business_type_group_cd = 'ID' then '商社・流通・小売・サービス'
+            when mbt.business_type_group_cd = 'IE' then '広告・マスコミ・エンターテインメント'
+            when mbt.business_type_group_cd = 'IF' then '金融・不動産・建設'
+            when mbt.business_type_group_cd = 'IG' then '医薬・医療・バイオ・メディカル'
+            when mbt.business_type_group_cd = 'IH' then 'インフラ・公共・その他'
+            else mbt.business_type_name end as business_group_type_name
+      ,mbt.business_type_name
+      ,ci.code_alias as income
+      ,ceb.code_alias as education_bg
+      ,r.job_change_duration_cd
+      ,r.toeic_score
+      ,r.toefl_score
+      ,r.company_count
+      ,LEN(NVL(rc.company_business, '') || NVL(rc.business_content, '') || NVL(rc.appeal_point, '') || NVL(rc.free_text, '')) as rsume_char_cnt
+      ,c.candidate_status_cd
+    from
+      candidate c
+        left join candidate_personal_info cpi on c.candidate_id = cpi.candidate_id
+        left join candidate_rating cr on c.candidate_id = cr.candidate_id
+        left join mst_area ma on c.area_cd = ma.mst_area_cd
+    
+        left join resume r on c.candidate_id = r.candidate_id
+          left join cls_education_bg ceb on r.education_bg_cd = ceb.education_bg_cd
+          left join cls_income ci on r.income_cd = ci.income_cd
+          left join (
+            -- resume_education_backgroundのresume_id重複削除
+            select
+              resume_id
+              ,education_bg_cd
+              ,education_name
+              ,graduation_year
+              ,course_type_cd
+              ,school_master_id
+              ,drop_flg
+              ,expected_flg
+            from
+              resume_education_background
+            where
+              display_no = '1'
+            group by
+              resume_id
+              ,education_bg_cd
+              ,education_name
+              ,graduation_year
+              ,course_type_cd
+              ,school_master_id
+              ,drop_flg
+              ,expected_flg
+            ) reb
+            on r.resume_id = reb.resume_id
+              left join mst_school_info msi on reb.school_master_id = msi.mst_school_info_id
+    
+          left join resume_company rc on r.resume_id = rc.resume_id and rc.display_no = '1'
+          left join resume_business rb on r.resume_id = rb.resume_id and rb.display_no = '1'
+            left join mst_business_type mbt on rb.resume_business_type_cd  = mbt.business_type_cd
+          left join resume_job rj on r.resume_id = rj.resume_id and rj.display_no = '1'
+            left join mst_job_type mjt on rj.resume_job_type_cd  = mjt.job_type_cd
+  )
+  
+  select
+    -- static
+    c.candidate_id as c_candidate_id
+    ,c.ins_datetime as c_reg_datetime
+    ,c.gender_cd as c_gender_cd
+    ,c.age as c_age
+    ,c.school_rank_cd as c_school_rank_cd
+    ,c.candidate_rank_cd as c_candidate_rank_cd
+    ,c.JOB_GROUP_TYPE_NAME as c_recent_g_job
+    ,c.job_type_name as c_recent_job
+    ,c.business_group_type_name as c_recent_g_industory
+    ,c.business_type_name as c_recent_industory
+    ,c.income as c_recent_income
+    ,c.education_bg as c_education_bg
+    ,c.job_change_duration_cd as c_job_change_duration_cd
+    ,c.rsume_char_cnt as c_rsume_char_cnt
+    ,c.candidate_status_cd as c_status_cd
+    ,c.trcd as c_trcd
+    ,c.area_name as c_area
+    ,c.toeic_score as c_toeic
+    ,c.toefl_score as c_toefl
+    ,c.company_count as c_com_exp_cnt
+  from
+    c 
 )
 
+-- merge 
 select
-  *
+  tran.candidate_id
+  ,tran.act_date
+  ,tran.act_type
+  ,tran.route_info
+  ,tran.job_id
+  ,tran.recruiter_company_id
+  ,tran.message_thread_id -- スカウト通数カウント用
+  ,tran.ps_reply_date
+  ,c2.c_reg_datetime
+  ,c2.c_gender_cd
+  ,c2.c_age
+  ,c2.c_school_rank_cd
+  ,c2.c_candidate_rank_cd
+  ,c2.c_recent_g_job
+  ,c2.c_recent_job
+  ,c2.c_recent_g_industory
+  ,c2.c_recent_industory
+  ,c2.c_recent_income
+  ,c2.c_education_bg
+  ,c2.c_job_change_duration_cd
+  ,c2.c_rsume_char_cnt
+  ,c2.c_status_cd
+  ,c2.c_trcd
+  ,c2.c_area
+  ,c2.c_toeic
+  ,c2.c_toefl
+  ,c2.c_com_exp_cnt
 from
   tran
+  left join c2 on tran.candidate_id = c2.c_candidate_id
